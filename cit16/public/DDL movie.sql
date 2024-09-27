4f876191c134f451a8ec546b2d3b2fa9d11620ca
Moosdorf <90873947+Moosdorf@users.noreply.github.com>
2024-09-27 15:39:37 +0200

    init push
    Everything we made is now on git and updated.

--- cit16/public/DDL movie.sql
+++ cit16/public/DDL movie.sql
@@ -1,5 +1,5 @@
 -- relations
-drop table if exists person_is_a;
+drop table if exists person_has_a;
 drop table if exists title_award;
 drop table if exists person_involved_title;
 drop table if exists title_is;
@@ -10,102 +10,91 @@ drop table if exists ep_title;
 -- entities
 drop table if exists genre;
 drop table if exists award;
-drop table if exists title;
-drop table if exists episode;
 drop table if exists movie;
+drop table if exists episode;
+drop table if exists title;
 drop table if exists profession;
 drop table if exists person;
 
 
 create table profession(
-
-	profession varchar(40) primary key
+	profession varchar(40) primary key not null
 );
 
-create table person(
 
+
+create table person(
 	p_id varchar(10) primary key,
 	name varchar(100),
 	birth_year numeric(4,0),
 	death_year numeric(4,0)
 );
 
-create table title(
+create table person_has_a(
+	profession varchar(40), 
+	p_id varchar(10),
+	primary key(p_id, profession),
+	foreign key (profession) references profession on update cascade,
+	foreign key (p_id) references person
+);
 
+
+create table title(
 	t_id varchar(10) primary key,
 	title varchar(2000),
 	rating numeric (4,2),
-	--type varchar(20),
+	type varchar(20),
 	isadult varchar(10),
 	released varchar(15),
 	language varchar(1000),
 	country varchar(1000),
+  runtime numeric(5,0),
 	poster varchar(1000)
 );
 
-
-create table genre(
-
-	genre varchar(30) primary key
-);
-
-
-create table award(
-
-	a_id varchar(10) primary key,
-	award_type varchar(30),
-	won varchar(1)
-);
-
-create table movie(
-
-	mov_id varchar (10) references title(t_id),
-  movtype varchar(100),
-  runtime numeric(5,0)
-);
-
-
-create table episode(
-
-	ep_id varchar (10) references title(t_id),
-	ep_name varchar(1000),
-	season_num numeric(2,0),
-	ep_num numeric(4,0),
-	runtime numeric(5,0),
-	rating numeric(4,2)
-);
-
-create table person_is_a(
-	
-	profession varchar(40), 
-	p_id varchar(10),
-	primary key(p_id, profession),
-	foreign key (profession) references profession,
-	foreign key (p_id) references person
-);
-
 create table person_involved_title(
-
-	in_id serial,
 	p_id varchar(10),
 	t_id varchar(10),
 	job varchar(30),
-	primary key(p_id, t_id, in_id),
+  character varchar(200) default null,
+	primary key(p_id, t_id, job),
 	foreign key(p_id) references person,
 	foreign key(t_id) references title
 );
 
-create table title_is(
+create table movie(
+	mov_id varchar (10) references title(t_id)
+);
 
+create table episode(
+	ep_id varchar (10) references title(t_id),
+	ep_name varchar(1000),
+	season_num numeric(2,0),
+	ep_num numeric(4,0),
+	rating numeric(4,2)
+);
+
+create table genre(
+	genre varchar(30) primary key
+);
+
+create table title_is(
 	genre varchar(30),
   t_id varchar(10),
 	primary key(genre, t_id),
-	foreign key(genre) references genre,
+	foreign key(genre) references genre on update cascade,
 	foreign key(t_id) references title
 );
 
-create table title_award(
 
+
+/*
+create table award(
+	a_id varchar(10) primary key,
+	award_type varchar(30),
+	won varchar(1)
+);
+create table title_award(
 	a_id varchar(10),
 	t_id varchar(10),
 	primary key(a_id, t_id),
@@ -114,8 +103,7 @@ create table title_award(
 );
 
 create table mov_title(
-
-	t_id varchar(10),
+	t_id varchar(10) not null,
 	mov_id varchar(10),
 	primary key(t_id, mov_id),
 	foreign key(t_id) references title,
@@ -123,10 +111,9 @@ create table mov_title(
 );
 
 create table ep_title(
-
 	ep_id varchar(10),
-	t_id varchar(10),
+	t_id varchar(10) not null,
 	primary key(ep_id, t_id),
 	foreign key(ep_id) references episode, 
 	foreign key(t_id) references title 
-);
+);*/
---
+++ cit16/public/Insert into Movie DB.sql
@@ -0,0 +1,219 @@
+/*
+DO $$
+declare rec record;
+month_num int;
+date int;
+begin
+for rec in select tconst, SUBSTR(released, 1,2) as "d", SUBSTR(released, 4,3) as "m", SUBSTR(released, 8,4) as "y" from omdb_data
+loop
+  
+  case 
+    when rec."m" = 'Jan' then month_num := 1;
+    when rec."m" = 'Feb' then month_num := 2;
+    when rec."m" = 'Mar' then month_num := 3;
+    when rec."m" = 'Apr' then month_num := 4;
+    when rec."m" = 'May' then month_num := 5;
+    when rec."m" = 'Jun' then month_num := 6;
+    when rec."m" = 'Jul' then month_num := 7;
+    when rec."m" = 'Aug' then month_num := 8;
+    when rec."m" = 'Sep' then month_num := 9;
+    when rec."m" = 'Oct' then month_num := 10;
+    when rec."m" = 'Nov' then month_num := 11;
+    when rec."m" = 'Dec' then month_num := 12;
+    else month_num := 00;
+    end case;
+    raise notice '% %', rec.tconst, month_num;
+
+end loop;
+END;
+$$;*/
+
+/* 
+ENTITIES CREATION
+*/
+
+
+
+
+/* PERSON TABLE */
+-- insert person data
+insert into person
+  select nconst, primaryname, cast(nullif(birthyear, '') as integer), cast(nullif(deathyear, '') as integer) from name_basics;
+  
+  
+  
+  
+/* PROFESSION TABLE */
+-- insert each profession, found in their person table
+-- we split up to make a profession out of each
+insert into profession
+select distinct unnest(string_to_array(name_basics.primaryprofession, ',')) from name_basics;
+ 
+ 
+ /* TITLE TABLE */
+insert into title
+select tconst, primarytitle, null, isadult, released, language, country, poster
+from title_basics left join omdb_data using(tconst);
+
+
+
+
+/* MOVIE TABLE */
+
+-- all "movies"
+insert into movie
+select tconst, titletype, runtimeminutes
+from title_basics left join omdb_data using(tconst)
+where titletype in ('tvShort', 'movie', 'tvMovie', 'video', 'short');
+
+
+
+
+/* EPISODE TABLE */
+
+-- all "episodes"
+insert into episode
+select tconst, primarytitle, cast(nullif(season, 'N/A') as numeric),  cast(nullif(episode, 'N/A') as numeric), runtimeminutes, null
+from title_basics left join omdb_data using(tconst)
+where titletype not in ('tvShort', 'movie', 'tvMovie', 'video', 'short', 'videoGame');
+
+
+/*
+-- all 'games' if usable
+insert into games????
+select tconst, primarytitle, null, isadult, released, language, country, poster
+from title_basics left join omdb_data using(tconst)
+where titletype = 'videoGame';
+*/
+
+
+
+/* GENRE TABLE */ 
+insert into genre
+(select distinct split_part(genres, ',', 1) as genre from title_basics
+where genres <> '' and genres <> 'N/A'
+
+union
+
+select distinct split_part(genre, ',', 1) as genre from omdb_data
+where genre <> '' and genre <> 'N/A');
+
+
+/* 
+RELATION CREATION
+*/
+
+
+
+
+/* PERSON_HAS_A TABLE */
+insert into person_has_a
+select unnest(string_to_array(name_basics.primaryprofession, ',')), nconst
+from name_basics;
+
+
+
+
+
+/* PERSON_INVOLVED_TITLE TABLE */
+insert into person_involved_title (p_id, t_id, job)
+select nconst, tconst, category
+from title_principals;
+
+
+
+/*select * from person_involved_title
+natural join person
+NATURAL join title 
+where name = 'Samuel L. Jackson';
+
+select *
+from title_basics
+join title_principals using (tconst)
+where nconst = 'nm0000168';*/
+
+
+
+/* TITLE_IS TABLE */
+insert into title_is
+select unnest(string_to_array(title_basics.genres, ',')), tconst
+from title_basics;
+
+
+
+/*
+select unnest(string_to_array(title_basics.genres, ',')), tconst
+from title_basics
+where tconst = 'tt21050232';*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- old
+
+/* EP_TITLE relation 
+
+--insert into ep_title
+insert into ep_title
+select tconst, parenttconst
+from title_episode join episode on ep_id = tconst;
+
+--select title, count(*) 
+--from ep_title join title using(t_id)
+--GROUP BY title;
+
+*/
+
+/* casting relation
+
+-- ep cast
+insert into casting (p_id, ep_id, character)
+select nconst, ep_id, characters
+from episode join title_principals on ep_id = tconst
+where category in ('actor', 'actress');
+--where ep_name = 'The One with the Ick Factor' and category in ('actor', 'actress');
+
+select * from casting;
+
+
+select characters from title_principals
+ORDER BY length(characters) desc;
+
+
+--mov cast
+--insert into casting (p_id, t_id, character)
+select nconst, mov_id, characters
+from movie
+join title_principals on mov_id = tconst
+where category in ('actor', 'actress');
+
+
+select * from casting
+where t_id is not null;
+
+select * from title_principals
+where nconst = 'nm0891514 ';
+
+
+ */
+
+
+
+
+
+
