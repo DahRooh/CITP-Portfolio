306abb123916fc199a3dfc67ead4c31c0be2814e
DahRooh <124369555+DahRooh@users.noreply.github.com>
2024-10-06 17:41:01 +0200

    I gang med testing + kommentarer

--- cit16/public/Testing.sql
+++ cit16/public/Testing.sql
@@ -8,26 +8,42 @@
 
 /*Testing: Sign up*/
 
--- 1: checking if any users exist
+-- 1: no users exist
 select * from users; 
 
--- 2: Calling our function
+-- 2: after calling our procedure
 call signup('username1', 'hashed-password', 'mail1@mail.ok', null);
 
--- 3: checking again if any users exist
+-- 3: one user exists
 select * from users;
 
 
 /*Testing: Login*/
 
 -- 1: Testing if a user can login
-select * from login_user('username1', 'hashed-password'); -- User can login
-
 
 select * from login_user('username1', 'incorrect-password'); -- User cannot login
 
+select * from get_session(1); -- no session started
+
+select * from login_user('username1', 'hashed-password'); -- User can login (session start)
+
+select * from get_session(1); -- session has been started
+-- skal med i rapport (ikke skrevet om denne)
+
+call sign_off(1); -- user signs off (session stops)
+-- skal med i rapport (ikke skrevet om denne)
+
+select * from get_session(1); -- session has ended (as seen my by timeended, not expired)
+-- skal med i rapport (ikke skrevet om denne)
+
+select * from login_user('username1', 'hashed-password'); -- User logs in again
 
 
+
+
+
+-- TO-DO:
 /*Testing: Search*/
 
 -- 1: Check if user has any search history
@@ -52,14 +68,22 @@ select * from get_bookmarks(1);
 -- 2: User bookmarks webpages
 call insert_bookmark(1, 'wptt2506874');
 call insert_bookmark(1, 'wptt0108778');
+call insert_bookmark(1, 'wpnm0000001');
 
 -- 3: User has bookmarked.
 select * from get_bookmarks(1);
 
-
 -- 1: Check how many users have bookmarked a webpage
 select * from get_bookmarks('wptt0108778'); 
 
+-- 2: Create new user (user 2)
+call signup('username2', 'hashed-password', 'mail2m@mail.ok', null);
+
+-- 3: User 2 creates a bookmark
+call insert_bookmark(2, 'wptt0108778');
+
+-- 3: User 2 has bookmarked
+select * from get_bookmarks(2);
 
 
 
