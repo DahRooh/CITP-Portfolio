671c49ebe724ce8f00ae49bbd0aa9a676cdf54ee
DahRooh <124369555+DahRooh@users.noreply.github.com>
2024-10-07 10:43:39 +0200

    Testfile is almost done

--- cit16/public/Testing.sql
+++ cit16/public/Testing.sql
@@ -22,11 +22,11 @@ select * from users;
 
 -- 1: Testing if a user can login
 
-select * from login_user('username1', 'incorrect-password'); -- User cannot login
+call login_user('username1', 'incorrect-password', null); -- User cannot login
 
 select * from get_session(1); -- no session started
 
-select * from login_user('username1', 'hashed-password'); -- User can login (session start)
+call login_user('username1', 'hashed-password', null); -- User can login (session start)
 
 select * from get_session(1); -- session has been started
 -- skal med i rapport (ikke skrevet om denne)
@@ -37,7 +37,7 @@ call sign_off(1); -- user signs off (session stops)
 select * from get_session(1); -- session has ended (as seen my by timeended, not expired)
 -- skal med i rapport (ikke skrevet om denne)
 
-select * from login_user('username1', 'hashed-password'); -- User logs in again
+call login_user('username1', 'hashed-password', null); -- User logs in again
 
 
 
@@ -64,6 +64,18 @@ select * from get_user_history(1);
 call clear_history(1);
 
 select * from get_user_history(1);
+
+/*Following section is centered upon searching algorithms*/
+
+-- The following is not used in the program:
+-- Finds movies in which the following substring is part of either the movie/series title or the plot description.
+-- It is a simple function which we expand upon later.
+select * from string_search('Godfath');
+-- A structured search algorithm. Finds titles.
+select * from structured_string_search('monKey', 'blob', 'Bilbo', 'Alfred');
+-- Following is based on "string_search". Finds prople instead of titles.
+select * from simple_search_person('friends');
+
 
 
 /*Testing: Bookmark*/
@@ -133,7 +145,9 @@ where t_id in ('tt2506874', 'tt0108778');
 
 -- 1 User can write a review
 call rate('tt21050232', 1, 2, 'Bad movie');
-select * from get_user_rating(1);
+select * from review
+natural join rates;
+
 
 -- 2 other user can like the review -- fejl
 call like_review(1,3,1); -- user 1 likes review 3. 1 for like -1 for dislike.
@@ -149,6 +163,14 @@ select * from review; -- fejl
 
 -- 4 User can edit his old review
 call rate('tt21050232', 1, 7, 'Better');
+
+
+
+
+
+
+
+
 
 
 
@@ -173,6 +195,45 @@ select * from get_session(1);
 
 
 select * from title order by rating desc limit 10;
+
+
+/*Testing: Person_known_for*/
+
+-- 1: Testing what titles a person has been involved in. 
+-- It can be actors, directors, etc.
+select person_known_for('Fred Astaire');
+select person_known_for('Alfred Hitchcock');
+
+
+/*Testing: find_coactors. */
+-- How many times have an actor/actress worked with another actor/actress.
+
+select * from find_coactors('Ian McKellen');
+
+/*Testing: name_ratings*/
+-- Finds average rating of people involved in specified movie/series. 
+
+select * from name_ratings('The Hobbit: The Desolation of Smaug');
+
+
+/*Testing: co_player_rating*/
+-- Finds rating of co-actors to the specified actor/actress
+select * from co_players_rating('Ian McKellen');
+
+-- find_similar_titles!!!
+
+/*Testing_ person_words*/
+-- Finds most frequent words related to the name passed to the function. 
+-- The interger determines the amount of words.
+select * from person_words('Ian McKellen', 10);
+
+/*Testing: exact_match*/
+-- All movies displayed are in some way related to all passed words. 
+-- The fewer the keywords, the more results, and the other way around. 
+
+select * from exact_match('monkey', 'king', 'queen'); -- returns titles
+select * from exact_match('monkey', 'king', 'queen', 'dog'); -- does not return titles due to 'dog'
+
 
 
 
