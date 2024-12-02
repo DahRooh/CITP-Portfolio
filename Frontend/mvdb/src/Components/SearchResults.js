d4060bd4e0fb56522fd35c1eb14a7a3a0bb21fac
DahRooh <124369555+DahRooh@users.noreply.github.com>
2024-12-02 12:49:23 +0100

    Work with database. Problems in SearchResult, extensive progress in SignIn.

--- Frontend/mvdb/src/Components/SearchResults.js
+++ Frontend/mvdb/src/Components/SearchResults.js
@@ -1,45 +1,57 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect, useState } from 'react';
 import { Container, Row, Col, Button, ButtonGroup } from 'react-bootstrap';
 
-function SearchResult() {
+function SearchResult({result, name}) {
+  return (
+    <Row>
+      <Col className="search" style={{paddingRight: "5vw"}}>
+        <img className="searchResultPictures" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpHrvCwx8k_WyIOri6iWKD443_4bR3_zwUCw&s"/>
+        <span style={{ display: "block", textAlign: "center", margin: "0 auto" }}>
+          {name}
+        </span>
+      </Col>
+    </Row>)
+}
+
+function SearchResults() {
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5001/api/search?keyword=spielberg&page=1&pageSize=20`)
+    .then(res => {
+      if (res.ok) return res.json(); 
+      return {};
+    })
+    .then(data => {
+      if (data) setSearchResults(data.items);
+      else new Error("No results found");
+    })
+    .catch(e => {
+      console.log(e)
+    })}, []);
+
+    let fake = ["search reasult 1", "search reasult 2", "search reasult 3"];
+    
+
   return (
 
     <div className="fullscreen">
+     
       <Container>
         <Row className="textHeader">
           <Col>
               Keyword: "keyword"
           </Col>
-        </Row>
 
+          
+
+        </Row>
           <Row className="search" style={{paddingLeft: "5vw", paddingRight: "5vw", paddingTop: "5vh"}}>
 
-            <Row>
-              <Col className="search" style={{paddingRight: "5vw"}}>
-                <img className="searchResultPictures" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpHrvCwx8k_WyIOri6iWKD443_4bR3_zwUCw&s"/>
-                <span style={{ display: "block", textAlign: "center", margin: "0 auto" }}>
-                  Search result 1
-                </span>
-              </Col>
-            </Row>
+            {fake.map(f => <SearchResult name={f}/>)}            
 
-            <Row>
-              <Col className="search" style={{paddingRight: "5vw"}}>
-                <img className="searchResultPictures" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpHrvCwx8k_WyIOri6iWKD443_4bR3_zwUCw&s"/>
-                <span style={{ display: "block", textAlign: "center", margin: "0 auto" }}>
-                  Search result 2
-                </span>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col className="search" style={{paddingRight: "5vw"}}>
-                <img className="searchResultPictures" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpHrvCwx8k_WyIOri6iWKD443_4bR3_zwUCw&s"/>
-                <span style={{ display: "block", textAlign: "center", margin: "0 auto" }}>
-                  Search result 3
-                </span>
-              </Col>
-            </Row>
           </Row>
 
           <Row style={{textAlign: "center"}}>
@@ -59,4 +71,4 @@ function SearchResult() {
   );
 }
   
-export default SearchResult;
+export default SearchResults;
--- Frontend/mvdb/src/Components/SignIn.js
+++ Frontend/mvdb/src/Components/SignIn.js
@@ -1,7 +1,47 @@
+/* (22)
+varierende variabler
+dem der kommer til at være en del af dep. array?
+hashing funktion - skal have salt. Skal gemme salt/standard salt som inge kender?
+*/
+
 import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect, useState } from 'react';
 import { Container, Row, Col, FormGroup, Button } from 'react-bootstrap';
 
 function SignIn() {
+
+  const [logInData, setLogInData] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:5001/api/user/sign_in`, {
+      method: "PUT", 
+      body: JSON.stringify(
+        {
+          username: "eray",
+          password: "eray"
+
+          
+        }
+      ),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => {
+      if (res.ok) return res.json();
+      return {};
+    })
+    .then(data => {
+      if (data.token) setLogInData(data);
+      else new Error("Cannot log in");
+    })
+    .catch(e => {
+      console.log(e)
+    })
+  }, []);
+
+  console.log(logInData)
+
   return (
 
     <div className="fullscreen">
